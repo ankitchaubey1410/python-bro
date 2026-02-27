89aeaacc686c46a3bdfcf3be08b92a40b99fdb45
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-02-27 18:20:20 +0530

    ADD string

---
+++ Chapter-3/string.py
@@ -0,0 +1,24 @@
+# name = input("Enter your full name: ")
+# phone_number = input("Enter your phone #: ")
+
+# result = len(name)
+# result = name.find("b")  # find the index of the first space character and character which is not found will return -1
+# result = name.rfind("b")  # find the index of the last space character
+# print(result)
+
+# name = name.capitalize() # it uppercase the first character and rest of all characters will be lowercase 
+# name= name.upper()# it uppercases all the characters in the string
+# name = name.lower()# it lowercases all the characters in the string
+# name = name.title()# it  uppercases the first character of word in the string
+# print(name)
+
+# result = name.isdigit() # it checks whether all the characters in the string are digits or not and return boolean value
+# result = name.isalpha() # it checks whether all the characters in the string are alphabets or not and return boolean value
+# result = name.isalnum() # it checks whether all the characters in the string are
+# result = phone_number.count("5") # it counts the number of times the specified character appears in the string
+# print(result)
+
+# phone_number = phone_number.replace("-", " ") # it replaces the specified character with the new character in the string and return a new string
+# print(phone_number)
+
+# print(help(str)) # it will show all the methods of string class
