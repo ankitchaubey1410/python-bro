d7a257b6f18e1eacb4881f3317c11096b9719297
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-02-28 12:35:07 +0530

    ADD indexing of a string

---
+++ Chapter-3/indexing.py
@@ -0,0 +1,7 @@
+# indexing = accessing elements of a sequence using [] (indexing operator)
+#            [start : end : step]
+
+credit_number = "1234-5678-9012-3456"
+print(credit_number[0])
+print(credit_number[0:5])
+print(credit_number[0:8:2])
