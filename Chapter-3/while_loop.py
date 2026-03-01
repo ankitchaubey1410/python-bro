a3166c0f21fad79748c8c2610de2e176b0afb0a5
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-03-01 12:48:03 +0530

    UPDATE while loop

--- Chapter-3/while_loop.py
+++ Chapter-3/while_loop.py
@@ -6,3 +6,29 @@ while name =="":
     print("You didn't enter your name!!")
     name = input("Enter your name: ")
 print(f"Hello {name}!!")
+
+
+age = int(input("Enter your age: "))
+
+while age < 0:
+    print("Age can't be negative!!")
+    age = int(input("Enter your age: "))
+print(f"You are {age} years old!!")
+
+
+food = input("Enter a food you like (q to quit): ")
+
+while not food == "q":
+    print(f"You like {food}")
+    food = input("Enter a food you like (q to quit): ")
+print("Byy Sweety!!🥰🫦")
+
+
+num = int(input("Enter a # between 1 - 10: "))
+
+while num < 1 or num > 10:
+    print(f"Your number : {num} is invalid!!")
+    num = int(input("Enter a # between 1 - 10: "))
+print(f"Your number is: {num}")
+
+
