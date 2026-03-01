ce93d880d9ddd22738562664d1a3bd2bca8cb28e
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-03-01 09:35:14 +0530

    UPDATE format specifier

--- Chapter-3/format_specifier.py
+++ Chapter-3/format_specifier.py
@@ -12,9 +12,9 @@
 # :  = insert a space before positive numbers
 # :, = comma separator
 
-price1 = 3.14159
-price2 = -987.65
-price3 = 12.34
+price1 = 3000.14159
+price2 = -9870.65
+price3 = 1200.34
 
 print(f"Price1 is : ${price1:.2f}")
 print(f"Price2 is : ${price2:.2f}")
@@ -23,23 +23,38 @@ print(f"Price3 is : ${price3:.2f}")
 # print(f"Price2 is : {price2:.3f}") #price2 = -987.650
 # print(f"Price3 is : {price3:.3f}") #price3 = 12.340
 
-print(f"Price1 is : ${price1:10}") #Each value now have 10 spaces and characters in it 
+print(f"Price1 is : ${price1:10}")   #Each value is now have 10 spaces and characters in it 
 print(f"Price2 is : ${price2:10}")
 print(f"Price3 is : ${price3:10}")
 
-print(f"Price1 is : ${price1:010}") #Each value is now have 10 spaces and each characters is filled with zero's
+print(f"Price1 is : ${price1:010}")  #Each value is now have 10 spaces and each characters is filled with zero's
 print(f"Price2 is : ${price2:010}") 
 print(f"Price3 is : ${price3:010}")
 
-print(f"Price1 is : ${price1:>10}") #Each value is now have 10 spaces and each characters is filled with zero's and right justified
+print(f"Price1 is : ${price1:>10}")  #Each value is now have 10 spaces and each characters is filled with zero's and right justified
 print(f"Price2 is : ${price2:>10}")
 print(f"Price3 is : ${price3:>10}")
 
-print(f"Price1 is : ${price1:<10}") #Each value is now have 10 spaces and each characters is filled with zero's and left justified
+print(f"Price1 is : ${price1:<10}")  #Each value is now have 10 spaces and each characters is filled with zero's and left justified
 print(f"Price2 is : ${price2:<10}")
 print(f"Price3 is : ${price3:<10}")
 
-print(f"Price1 is : ${price1:^10}") #Each value is now have 10 spaces and each characters is filled with zero's and center justified
+print(f"Price1 is : ${price1:^10}")  #Each value is now have 10 spaces and each characters is filled with zero's and center justified
 print(f"Price2 is : ${price2:^10}")
 print(f"Price3 is : ${price3:^10}")
 
+print(f"Price1 is : ${price1:+}")    #Each value shows a plus sign if it's positive and a minus sign if it's negative
+print(f"Price2 is : ${price2:+}")
+print(f"Price3 is : ${price3:+}")
+
+print(f"Price1 is : ${price1: }")    #Each value shows a space in place of positive sign
+print(f"Price2 is : ${price2: }")
+print(f"Price3 is : ${price3: }")
+
+print(f"Price1 is : ${price1:,}")    #Each value shows a comma separator
+print(f"Price2 is : ${price2:,}")
+print(f"Price3 is : ${price3:,}")
+
+print(f"Price1 is : ${price1:+,.2f}")    #Each value shows a plus sign if it's positive and a minus sign if it's negative, rounded to 2 decimal places and with a comma separator
+print(f"Price2 is : ${price2:+,.2f}")
+print(f"Price3 is : ${price3:+,.2f}")
