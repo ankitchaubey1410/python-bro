f2bdc94196e862b5b87d72de4c9584a137a1e6e0
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-03-03 00:16:30 +0530

    add 2d lists

---
+++ Chapter-5/2d_lists.py
@@ -0,0 +1,14 @@
+# 2D Lists
+
+fruits = ["apple", "orange", "banana", "coconut"]
+vegetables = ["celery", "carrots", "potatoes"]
+meats =["chicken", "fish", "turkey"]
+
+groceries = [fruits, vegetables, meats]
+
+
+# print(fruits)
+# print(vegetables)
+# print(meats)
+
+print(groceries)
