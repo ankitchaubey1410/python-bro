f9c2918eab7c2d60347bfcea6943f5efc0449f1e
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-03-11 00:32:37 +0530

    update 2d lists

--- Chapter-5/2d_lists.py
+++ Chapter-5/2d_lists.py
@@ -10,5 +10,21 @@ groceries = [fruits, vegetables, meats]
 # print(fruits)
 # print(vegetables)
 # print(meats)
+print(groceries[0]) # ['apple', 'orange', 'banana', 'coconut']
+print(groceries[1]) # ['celery', 'carrots', 'potatoes']
+print(groceries[2]) # ['chicken', 'fish', 'turkey']
+print(groceries) 
 
-print(groceries)
+print(groceries[0][0]) # apple
+print(groceries[0][1]) # orange
+print(groceries[1][0]) # celery
+print(groceries[1][2]) # potatoes
+# print(groceries[1][3]) # out of index
+print(groceries[2][1]) # fish
+print(groceries[2][2]) # turkey
+
+
+favorites = [["sabrina carpenter", "katy perry", "lana del rey"], ["scarlett johansson", "megan fox", "jennifer lawrence", "ana de armas"], ["deepika padukone","ayesha takia", "sunny leone", "mallika sherawat", "urvashi rautela"]]
+print(favorites)
+
+
