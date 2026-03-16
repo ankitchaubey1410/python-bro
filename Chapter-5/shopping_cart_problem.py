23bfeae9e8676e0b4d9c64210c34a8cbf264faa0
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-03-16 23:44:06 +0530

    add shopping cart problem

---
+++ Chapter-5/shopping_cart_problem.py
@@ -0,0 +1,18 @@
+# Shopping Cart Problem
+
+foods = []
+prices = []
+total = 0
+
+while True:
+    food = input("What food would you like to buy ? (q to quit) : ")
+    if food.lower() == "q":
+        break
+    else:
+        price = float(input(f"Enter the price of a {food} : $"))
+        foods.append(food)
+        prices.append(price)
+print("------ YOUR CART ------")
+
+for food in foods:
+    print(food, end=" ")
