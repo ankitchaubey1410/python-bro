0f3ede4bd984ad202982f97b104cbc901129cf64
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-03-19 01:25:06 +0530

    update quiz game

--- Chapter-5/quiz_game.py
+++ Chapter-5/quiz_game.py
@@ -15,3 +15,20 @@ answers = ("C", "D", "A", "A", "B")
 guesses = []
 score = 0
 question_num = 0
+
+for question in questions:
+    print("------------------------------------------------------")
+    print(question)
+    for option in options[question_num]:
+        print(option)
+
+    guess = input("Enter (A, B, C, D): ").upper()
+    guesses.append(guess)
+
+    if guess == answers[question_num]:
+        score += 1
+        print("CORRECT BABY😘")
+    else:
+        print("INCORRECT BITCH😓")
+        print(f"{answers[question_num]} is the correct answer")
+    question_num += 1
