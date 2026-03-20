5aff1f3adca25c303b0a6082aaca04a0ccf5eeef
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-03-20 12:28:56 +0530

    update match case program

--- Chapter-1/match_case.py
+++ Chapter-1/match_case.py
@@ -15,4 +15,22 @@ match n:
     case _:
         print("number is ",n)
 '''
-
+# Check 
+x = input("enter True and False if you are going for vote : ").capitalize()
+match x:
+    case "True":
+        print("Who you are going to vote 🧐? : ")
+        y = input("enter a party name (kitty or bitchy)? : ")
+        match y:
+            case "kitty":
+                print("you are going to vote kitty party 😻")
+            case "bitchy":
+                print("you are going to vote bitchy party 👧")
+    case "False":
+        print("you are not going to vote ? why ? : ")
+        z = int(input("enter your age : "))
+        match z:
+            case _ if (z <= 18):
+                print("ohh you are nihal bitch!! 🦋")
+            case _ if (z > 18):
+                print("ohh you are definetly love kirat!! 👨")
