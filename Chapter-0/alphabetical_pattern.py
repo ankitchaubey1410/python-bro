922c59a6f081a8124a782b93b66cfd6e9e99fa7b
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-03-20 13:04:08 +0530

    update alphabetical pattern printing

--- Chapter-0/alphabetical_pattern.py
+++ Chapter-0/alphabetical_pattern.py
@@ -1,4 +1,9 @@
 x = int(input("enter a number : "))
+# print(ord('a'))
+# it is used to find ascii value of that character
+# print(chr(65))
+# it is used to check what character is in that ascii value
+
 
 for i in range(0,x,1):
     for j in range(0,x,1):
@@ -11,4 +16,40 @@ print()
 for i in range(0,x,1):
     for j in range(0,x,1):
         print(chr(j+65),end=" ")
+    print()
+
+print()
+print()
+
+for i in range(0,x,1):
+    for j in range(0,i+1,1):
+        print(chr(j+65),end=" ")
+    print()
+
+print()
+print()
+
+for i in range(0,x,1):
+    for j in range(0,i+1,1):
+        print(chr(i+65),end=" ")
+    print()
+
+print()
+print()
+
+num = 65
+for i in range(0,x,1):
+    for j in range(0,i+1,1):
+        print(chr(num),end=" ")
+        num += 1
+    print()
+
+print()
+print()
+
+num = 65
+for i in range(0,x,1):
+    for j in range(0,x,1):
+        print(chr(num),end=" ")
+        num += 1
     print()
