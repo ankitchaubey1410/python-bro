8408314f392da20afa9b8dfcb8ae740bfb1dd708
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-03-20 12:44:07 +0530

    add chapter pattern printing

---
+++ Chapter-0/square_pattern.py
@@ -0,0 +1,5 @@
+x = int(input("enter a number : "))
+for i in range(0, x, 1):
+    for j in range(0, x, 1):
+        print("😘",end="  ")
+    print()
---
+++ Chapter-0/triangle_pattern.py
@@ -0,0 +1,6 @@
+x = int(input("enter a number : "))
+for i in range(0, x, 1):
+    for j in range(0, i+1, 1):
+        print("🤤", end="  ")
+    print()
+    print()
