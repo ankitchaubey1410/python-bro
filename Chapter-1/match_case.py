a4db6abe3b1215bb8e19cbe9c2826b6ed45bff31
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-03-20 12:01:08 +0530

    add match case program

---
+++ Chapter-1/match_case.py
@@ -0,0 +1,18 @@
+# Match Case Program
+'''
+n = int(input("enter a number : "))
+match n:
+    case 0:
+        print("number is zero")
+    case 1:
+        print("number is one")
+    case 2:
+        print("number is two")
+    case 3:
+        print("number is three")
+    case 4:
+        print("number is four")
+    case _:
+        print("number is ",n)
+'''
+
